6f48c47cbe82b98a000f23ae1aa4b0c23fb55269
Jeremy McPeak <jwmcpeak@gmail.com>
2024-06-13 22:48:48 -0500

    episode 4

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,7 +1,32 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
+use App\Models\Account;
+use App\Models\User;
+use Illuminate\Support\Facades\Context;
 
-    $response->assertStatus(200);
+beforeEach(function() {
+    $user = User::factory()->create();
+    $account = Account::factory()->create([
+        'name' => "Foo's Guitars"
+    ]);
+
+    $user->accounts()->save($account);
+
+    // Context::add('active_account', $account);
+    $this->actingAs($user)->withSession(['active_account' => $account]);
 });
+
+
+it('dashboard displays the account name', function() {
+    $this->get('/dashboard')
+        ->assertOk()
+        ->assertSee("Foo's Guitars");
+});
+
+
+
+// it('returns a successful response', function () {
+//     $response = $this->get('/');
+
+//     $response->assertStatus(200);
+// });
